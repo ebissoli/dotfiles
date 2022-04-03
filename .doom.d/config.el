adbd1997d8ba6787c06e468b76e1de7494f95129
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-03 11:56:48 -0300

    ADD to config.org PROGRAMMING section the Latex subsection

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -207,50 +207,7 @@ latter - its output."
 
 
 (setq +latex-viewers '(pdf-tools)) ;; programming -latex
-
-;; lsp-julia config
-(setq lsp-julia-package-dir nil)
-(setq lsp-julia-default-environment "~/.julia/environments/v1.0") ;; programming - julia
-
-(use-package! zig-mode
-  :hook ((zig-mode . lsp-deferred))
-  :custom (zig-format-on-save nil)
-  :config
-  (after! lsp-mode
-    (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
-    (lsp-register-client
-      (make-lsp-client
-        :new-connection (lsp-stdio-connection "/home/enzobissoli/zls/zls")
-        :major-modes '(zig-mode)
-        :server-id 'zls)))) ;; programming zig, can remove?
-
-
-(setq lsp-zig-zls-executable "~/.local/bin/zls") ;; programming - zig
-
-(set-docsets! 'c-mode "C")
-
-(set-docsets! 'sh-mode "Bash")
-
-(add-to-list 'load-path "/usr/bin/maxima/")
-(autoload 'maxima-mode "maxima" "Maxima mode" t)
-(autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
-(autoload 'maxima "maxima" "Maxima interaction" t)
-(autoload 'imath-mode "imath" "Imath mode for math formula input" t)
-(setq imaxima-use-maxima-mode-flag t)
-(add-to-list 'auto-mode-alist '("\\.ma[cx]\\'" . maxima-mode))
-(matlab-cedet-setup) ;;programming -maxima
-
-(setq exec-path (cons "/usr/local/SMLROOT/bin"  exec-path)) ;; programing sml
-
-(set-docsets! 'matlab-mode "MATLAB") ;; programming - misc, docsets?
-
-(require 'platformio-mode)
-
-;; Enable ccls for all c++ files, and platformio-mode only
-;; when needed (platformio.ini present in project root).
-(add-hook 'c++-mode-hook (lambda ()
-                           (lsp-deferred)
-                           (platformio-conditionally-enable)))
+#+begin_src elisp
 
 ;;;###autoload
 (defun keychain-refresh-environment ()
