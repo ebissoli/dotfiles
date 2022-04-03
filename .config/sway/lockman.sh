53c8b52fa79cfebfa1186417220e5ff5311ce7d8
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-03 10:26:43 -0300

    ADD sway/

---
+++ .config/sway/lockman.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+swayidle \
+    timeout 10 'swaymsg "output * dpms off" && playerctl -a pause' \
+    resume 'swaymsg "output * dpms on" && playerctl -a play' \
+    unlock 'playerctl -a pause' &
+swaylock -i "$HOME/.dotfiles/Wallpaper/dragon.png"
+kill %%
